3460b2663224e2389081bd35c2aa62f7b7ad171e
rajatkhanna <legalimpurity@gmail.com>
2017-09-03 13:32:27 +0530

    read the project wrongly, then tried git to restore it to an earlier state, screwed me up

--- backend/src/main/java/com/legalimpurity/backend/MyEndpoint.java
+++ backend/src/main/java/com/legalimpurity/backend/MyEndpoint.java
@@ -6,11 +6,10 @@
 
 package com.legalimpurity.backend;
 
+import com.example.jokesSourceClass;
 import com.google.api.server.spi.config.Api;
 import com.google.api.server.spi.config.ApiMethod;
 import com.google.api.server.spi.config.ApiNamespace;
-
-import java.util.Random;
 
 import javax.inject.Named;
 
@@ -35,22 +34,12 @@ public class MyEndpoint {
     public MyBean sayHi(@Named("name") String name) {
         MyBean response = new MyBean();
         if(name.equalsIgnoreCase("tell a joke"))
-            response.setData(getJoke());
+            response.setData(jokesSourceClass.getJoke());
         else
             response.setData("Hi, "+name);
         return response;
     }
 
-    public static String getJoke(){
-        String[] jokearray =  new String[]{
-                "Q: What computer sings the best?\nA: A Dell.",
-                "Q: How many light bulbs does it take to change a light bulb?\nA: They can't; they're not bright enough.",
-                "I put so much more effort into naming my first Wi-Fi than my first child.",
-                "The cool part about naming your kid is you don’t have to add six numbers to make sure the name is available."
-        };
 
-        Random rn = new Random();
-        return jokearray[rn.nextInt(jokearray.length)];
-    }
 
 }
--- jokessource/src/main/java/com/example/jokesSourceClass.java
+++ jokessource/src/main/java/com/example/jokesSourceClass.java
@@ -1,52 +1,18 @@
 package com.example;
 
-import java.io.BufferedReader;
-import java.io.DataOutputStream;
-import java.io.InputStream;
-import java.io.InputStreamReader;
-import java.net.HttpURLConnection;
-import java.net.URL;
 import java.util.Random;
 
 public class jokesSourceClass {
 
+    public static String getJoke(){
+        String[] jokearray =  new String[]{
+                "Q: What computer sings the best?\nA: A Dell.",
+                "Q: How many light bulbs does it take to change a light bulb?\nA: They can't; they're not bright enough.",
+                "I put so much more effort into naming my first Wi-Fi than my first child.",
+                "The cool part about naming your kid is you don’t have to add six numbers to make sure the name is available."
+        };
 
-    public static void getJoke(final JokeCallback call) {
-        new Thread(new Runnable() {
-            public void run() {
-                executePost(call,"http://192.168.0.3:8080/_ah/api/myApi/v1/sayHi/tell a joke");
-            }
-        }).start();
-    }
-
-    private static void executePost(JokeCallback call, String targetURL) {
-        HttpURLConnection connection = null;
-
-        try {
-            URL url = new URL(targetURL);
-            connection = (HttpURLConnection) url.openConnection();
-            connection.setRequestMethod("POST");
-
-            connection.setUseCaches(false);
-            connection.setDoOutput(true);
-
-            InputStream is = connection.getInputStream();
-            BufferedReader rd = new BufferedReader(new InputStreamReader(is));
-            StringBuilder response = new StringBuilder(); // or StringBuffer if Java version 5+
-            String line;
-            while ((line = rd.readLine()) != null) {
-                response.append(line);
-                response.append('\r');
-            }
-            rd.close();
-            call.returnLatestJoke(response.toString());
-        } catch (Exception e) {
-            e.printStackTrace();
-            call.returnLatestJoke("");
-        } finally {
-            if (connection != null) {
-                connection.disconnect();
-            }
-        }
+        Random rn = new Random();
+        return jokearray[rn.nextInt(jokearray.length)];
     }
 }
